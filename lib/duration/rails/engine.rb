ded376db57a531f41523355baee82a4c260db78c
Kristian Mandrup <kmandrup@gmail.com>
2012-05-07 11:17:47 +0200

    only add to locale path when files found

--- lib/duration/rails/engine.rb
+++ lib/duration/rails/engine.rb
@@ -2,7 +2,11 @@ module RubyDuration
 	module Rails
 		class Engine < ::Rails::Engine
 			initializer 'duration setup' do
-				I18n.load_path << Dir[::Rails.root.join('config', 'locales', 'duration', '*.yml')]
+				I18n.load_path << locale_files unless locale_files.blank?
+			end
+
+			def self.locale_files
+				@locale_files ||= Dir[::Rails.root.join('config', 'locales', 'duration', '*.yml')]
 			end
 		end
 	end
--- lib/duration/version.rb
+++ lib/duration/version.rb
@@ -1,3 +1,3 @@
 class Duration
-  VERSION = "2.2.2"
+  VERSION = "2.2.3"
 end
