0a79600bb026b0fa56847dd1f50b677be26fb115
Kristian Mandrup <kmandrup@gmail.com>
2012-05-07 11:22:21 +0200

    fix locale_path add

--- lib/duration/rails/engine.rb
+++ lib/duration/rails/engine.rb
@@ -2,11 +2,17 @@ module RubyDuration
 	module Rails
 		class Engine < ::Rails::Engine
 			initializer 'duration setup' do
-				I18n.load_path << locale_files unless locale_files.blank?
+				unless locale_files.blank?
+					locale_files.each do |file|
+						I18n.load_path << file
+					end
+				end
 			end
 
-			def self.locale_files
+			def locale_files
 				@locale_files ||= Dir[::Rails.root.join('config', 'locales', 'duration', '*.yml')]
+			rescue
+				[]
 			end
 		end
 	end
